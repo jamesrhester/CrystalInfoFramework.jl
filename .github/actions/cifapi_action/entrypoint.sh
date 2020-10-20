50d3c39e694ac49719e02637814aaa3fca7d4771
James.Hester <jxh@ansto.gov.au>
2020-10-20 19:09:30 +1100

    Added ICU libraries

--- .github/actions/cifapi_action/entrypoint.sh
+++ .github/actions/cifapi_action/entrypoint.sh
@@ -6,3 +6,9 @@ git clone https://github.com/COMCIFS/cif_api cif_api
 pushd cif_api
 ./configure --without-docs --prefix=$GITHUB_WORKSPACE
 make install
+
+# latest icu libraries
+cp -n `find /usr/lib -name libicuio.so*` $GITHUB_WORKSPACE/lib
+cp -n `find /usr/lib -name libicui18n.so*` $GITHUB_WORKSPACE/lib
+cp -n `find /usr/lib -name libicuuc.so*` $GITHUB_WORKSPACE/lib
+cp -n `find /usr/lib -name libicudata.so*` $GITHUB_WORKSPACE/lib
